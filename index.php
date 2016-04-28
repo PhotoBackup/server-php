bd5d4f848a153814d18165f220abab4b23118c3d
Martijn van der Ven <martijn@vanderven.se>
2016-04-28 08:24:17 +0200

    Write to a logfile for debugging.

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
  */
 
 /**
+ * The log file.
+ *
+ * This log file path is relative to this index.php file. Make sure that the
+ * file exists and the server has been given writing rights.
+ *
+ * @global string $Log A string (encapsulated by quotes).
+ */
+$Log = 'photobackup.log';
+
+/**
  * The password required to upload to this server.
  *
  * The password is currently stored as clear text here. PHP code is not normally
@@ -34,6 +44,16 @@ $MediaRoot = 'photos';
 // NO CONFIGURATION NECCESSARY BEYOND THIS POINT.
 // -----------------------------------------------------------------------------
 
+/** @global $logable This will be true if we can use logging. */
+$logable = false;
+
+/**
+ * Check if we can write logs.
+ **/
+if (is_writable($Log)) {
+    $logable = true;
+}
+
 /**
  * Establish what HTTP version is being used by the server.
  */
@@ -42,18 +62,27 @@ if (isset($_SERVER['SERVER_PROTOCOL'])) {
 } else {
     $protocol = 'HTTP/1.0';
 }
+if ($logable) {
+    file_put_contents($Log, "Server protocol set to: $protocol\n", FILE_APPEND);
+}
 
 /**
  * Find out if the client is requesting the test page.
  */
 $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $testing = substr($request, -5) === '/test';
+if ($logable && $testing) {
+    file_put_contents($Log, "We are currently running a test.\n", FILE_APPEND);
+}
 
 /**
  * If no data has been POSTed to the server and the client did not request the
  * test page, exit imidiately.
  */
 if (empty($_POST) && !$testing) {
+    if ($logable) {
+        file_put_contents($Log, "Nothing was submitted to the server.\nExiting.\n\n", FILE_APPEND);
+    }
     exit();
 }
 
@@ -68,6 +97,9 @@ if (
         !is_string($Password)
     )
 ) {
+    if ($logable) {
+        file_put_contents($Log, "The server is misconfigured. No password has been set.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 401 Unauthorized');
     exit();
 }
@@ -83,6 +115,9 @@ if (
     !isset($_POST['password']) ||
     $_POST['password'] !== hash('sha512', $Password)
 ) {
+    if ($logable) {
+        file_put_contents($Log, "The password sent by the client did not match the one on the server.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 403 Forbidden');
     exit();
 }
@@ -98,6 +133,9 @@ if (
     !is_dir($MediaRoot) ||
     !is_writable($MediaRoot)
 ) {
+    if ($logable) {
+        file_put_contents($Log, "The server is misconfigured. It cannot write to the MediaRoot.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 500 Internal Server Error');
     exit();
 }
@@ -106,6 +144,9 @@ if (
  * If we were only supposed to test the server, end here.
  */
 if ($testing) {
+    if ($logable) {
+        file_put_contents($Log, "Testing completed.\nExiting.\n\n", FILE_APPEND);
+    }
     exit();
 }
 
@@ -113,6 +154,9 @@ if ($testing) {
  * If the client did not submit a filesize, exit with HTTP code 400.
  */
 if (!isset($_POST['filesize'])) {
+    if ($logable) {
+        file_put_contents($Log, "The client did not sent the filesize parameter.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 400 Bad Request');
     exit();
 }
@@ -126,6 +170,9 @@ if (
     $_FILES['upfile']['error'] !== UPLOAD_ERR_OK ||
     !is_uploaded_file($_FILES['upfile']['tmp_name'])
 ) {
+    if ($logable) {
+        file_put_contents($Log, "Th upload failed at some basic level.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 401 Unauthorized');
     exit();
 }
@@ -135,6 +182,9 @@ if (
  * with HTTP code 411.
  */
 if (intval($_POST['filesize']) !== $_FILES['upfile']['size']) {
+    if ($logable) {
+        file_put_contents($Log, "A mismatch in file sizes was detected.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 411 Length Required');
     exit();
 }
@@ -146,6 +196,9 @@ if (intval($_POST['filesize']) !== $_FILES['upfile']['size']) {
 $filename = preg_replace('@\s+@', '-', $_FILES['upfile']['name']);
 $filename = preg_replace('@[^0-9a-z._-]@i', '', $filename);
 $target = $MediaRoot . '/' . $filename;
+if ($logable) {
+    file_put_contents($Log, "Established server-side filename as $target.\n", FILE_APPEND);
+}
 
 /**
  * If a file with the same name and size exists, treat the new upload as a
@@ -155,6 +208,9 @@ if (
     file_exists($target) &&
     filesize() === $_POST['filesize']
 ) {
+    if ($logable) {
+        file_put_contents($Log, "There is already a server-side file with that name.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 409 Conflict');
     exit();
 }
@@ -164,8 +220,15 @@ if (
  * exit with HTTP code 500.
  */
 if (!move_uploaded_file($_FILES["upfile"]["tmp_name"], $target)) {
+    if ($logable) {
+        file_put_contents($Log, "The server was unable to move the uploaded file to its intended target location.\nExiting.\n\n", FILE_APPEND);
+    }
     header($protocol . ' 500 Internal Server Error');
     exit();
+}
+
+if ($logable) {
+    file_put_contents($Log, "Everything should have gone just peachy.\nExiting.\n\n", FILE_APPEND);
 }
 
 exit();
